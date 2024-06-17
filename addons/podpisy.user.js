ac4f5ed719c94e35bb4a26e653fe4bc3dd75bb7f
vumocode <91375898+vumocode@users.noreply.github.com>
2024-06-17 22:39:43 +0200

    Create podpisy.user.js

---
+++ addons/podpisy.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Podpisy KCS
+// @version      1.5
+// @description  Podpisy KCS'ów
+// @author       vumo
+// @match        https://*.margonem.pl/
+// @downloadURL  https://vumocode.github.io/margonem-addons/addons/podpisy.user.js
+// @updateURL    https://vumocode.github.io/margonem-addons/addons/podpisy.user.js
+// @require      https://vumocode.github.io/margonem-addons/addons/podpisy.user.js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=margonem.pl
+// @grant        none
+// ==/UserScript==
