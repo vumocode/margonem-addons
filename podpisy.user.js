eda943dacac16649dfe9ad5cd7076b865e9fcb79
vumocode <91375898+vumocode@users.noreply.github.com>
2024-06-17 22:40:49 +0200

    Create podpisy.user.js

---
+++ podpisy.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Podpisy KCS
+// @namespace    http://tampermonkey.net/
+// @version      1.5
+// @description  Podpisy KCS'ów
+// @author       You
+// @match        https://*.margonem.pl/
+// @downloadURL  https://vumocode.github.io/margonem-addons/addons/podpisy.js
+// @updateURL    https://vumocode.github.io/margonem-addons/addons/podpisy.js
+// @require      https://vumocode.github.io/margonem-addons/addons/podpisy.js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=margonem.pl
+// @grant        none
+// ==/UserScript==
