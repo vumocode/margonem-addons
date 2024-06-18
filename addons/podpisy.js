682a73df92650a95d5769b00f3ce2494cde947d2
vumocode <91375898+vumocode@users.noreply.github.com>
2024-06-18 11:11:34 +0200

    Update podpisy.js

--- addons/podpisy.js
+++ addons/podpisy.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Podpisy KCS
-// @version      1.02
+// @version      1.03
 // @description  Podpisy KCS'ów
 // @author       vumo
 // @match        https://*.margonem.pl/
