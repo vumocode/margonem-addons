3b68123502107b9bfae7a60cd78d428917cf603d
vumocode <91375898+vumocode@users.noreply.github.com>
2024-06-22 22:32:14 +0200

    Create betterll.user.js

---
+++ betterll.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Better Lootlog
+// @version      1.0
+// @description  Customowy styl css dla Lootloga.
+// @author       You
+// @match        https://*.margonem.pl/
+// @downloadURL  https://vumocode.github.io/margonem-addons/addons/betterll.js
+// @updateURL    https://vumocode.github.io/margonem-addons/addons/betterll.js
+// @require      https://vumocode.github.io/margonem-addons/addons/betterll.js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=margonem.pl
+// @grant        none
+// ==/UserScript==
