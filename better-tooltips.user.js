5642308cc11a343f6bad47d472ee30c9526914ce
vumocode <91375898+vumocode@users.noreply.github.com>
2024-07-05 14:10:43 +0200

    Create better-tooltips.user.js

---
+++ better-tooltips.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Better Tooltips
+// @version      1.0
+// @description  Customowy styl css tooltipów.
+// @author       vumo
+// @match        https://*.margonem.pl/
+// @downloadURL  https://vumocode.github.io/margonem-addons/addons/better-tooltips.js
+// @updateURL    https://vumocode.github.io/margonem-addons/addons/better-tooltips.js
+// @require      https://vumocode.github.io/margonem-addons/addons/better-tooltips.js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=margonem.pl
+// @grant        none
+// ==/UserScript==
