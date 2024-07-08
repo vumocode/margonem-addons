9c8cb610bec7ca64e3e439be24cc07e9f982af62
vumocode <91375898+vumocode@users.noreply.github.com>
2024-07-08 19:21:50 +0200

    Create gui-fix.user.js

---
+++ gui-fix.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         GUI FIX
+// @version      1.0
+// @description  Zmienia czerwone kolorki w GUI Margonem na szare.
+// @author       vumo
+// @match        https://*.margonem.pl/
+// @downloadURL  https://vumocode.github.io/margonem-addons/addons/gui-fix.js
+// @updateURL    https://vumocode.github.io/margonem-addons/addons/gui-fix.js
+// @require      https://vumocode.github.io/margonem-addons/addons/gui-fix.js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=margonem.pl
+// @grant        none
+// ==/UserScript==
