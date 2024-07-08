84a880ee672e281574043077e9d1b8a1f9342504
vumocode <91375898+vumocode@users.noreply.github.com>
2024-07-08 19:23:20 +0200

    Update and rename gui-fix.user.js to better-gui.user.js

--- better-gui.user.js
+++ better-gui.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name         GUI FIX
+// @name         Better GUI
 // @version      1.0
 // @description  Zmienia czerwone kolorki w GUI Margonem na szare.
 // @author       vumo
 // @match        https://*.margonem.pl/
-// @downloadURL  https://vumocode.github.io/margonem-addons/addons/gui-fix.js
-// @updateURL    https://vumocode.github.io/margonem-addons/addons/gui-fix.js
-// @require      https://vumocode.github.io/margonem-addons/addons/gui-fix.js
+// @downloadURL  https://vumocode.github.io/margonem-addons/addons/better-gui.js
+// @updateURL    https://vumocode.github.io/margonem-addons/addons/better-gui.js
+// @require      https://vumocode.github.io/margonem-addons/addons/better-gui.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=margonem.pl
 // @grant        none
 // ==/UserScript==
