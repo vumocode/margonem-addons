2a77df2656dee06a563473233927eb9bbb35eddc
vumocode <91375898+vumocode@users.noreply.github.com>
2024-07-08 18:05:53 +0200

    Create better-text.user.js

---
+++ better-text.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Better Text
+// @version      1.0
+// @description  Lepszy, nieklikalny tekst powiadomień. (Działa tylko z zestawem dodatków Maddonz)
+// @author       vumo
+// @match        https://*.margonem.pl/
+// @downloadURL  https://vumocode.github.io/margonem-addons/addons/better-text.js
+// @updateURL    https://vumocode.github.io/margonem-addons/addons/better-text.js
+// @require      https://vumocode.github.io/margonem-addons/addons/better-text.js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=margonem.pl
+// @grant        none
+// ==/UserScript==
